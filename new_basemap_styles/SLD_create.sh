fe46c8fd09242306e64c59ad812644c6b78d4042
jmiller@boundlessgeo.com <jmiller@boundlessgeo.com>
2016-09-06 20:44:11 -0400

    got rid of ysld loop

--- new_basemap_styles/SLD_create.sh
+++ new_basemap_styles/SLD_create.sh
@@ -36,34 +36,5 @@ for sldfile in *.sld; do
 
 done
 
-for ysldfile in *.ysld; do
-
-  # strip the extension from the filename to use for layer/style names
-  layername=`basename $ysldfile .ysld`
-
-  # create a new featuretype in the store, assuming the table
-  # already exists in the database and is named $layername
-  # this step automatically creates a layer of the same name
-  # as a side effect
-  curl -v -u $login -XPOST -H "Content-type: text/xml" \
-    -d "<featureType><name>$layername</name></featureType>" \
-    $restapi/workspaces/$workspace/datastores/$store/featuretypes?recalculate=nativebbox,latlonbbox
-
-  # create an empty style object in the workspace, using the same name
-  curl -v -u $login -XPOST -H "Content-type: text/xml" \
-    -d "<style><name>$layername</name><filename>$ysldfile</filename></style>" \
-    $restapi/workspaces/$workspace/styles
-
-  # upload the SLD definition to the style
-  curl -v -u $login -XPUT -H "Content-type: application/vnd.ogc.sld+xml" \
-    -d @$ysldfile \
-    $restapi/workspaces/$workspace/styles/$layername
-
-  # associate the style with the layer as the default style
-  curl -v -u $login -XPUT -H "Content-type: text/xml" \
-    -d "<layer><enabled>true</enabled><defaultStyle><name>$layername</name><workspace>$workspace</workspace></defaultStyle></layer>" \
-    $restapi/layers/$workspace:$layername
-
-done
 
   curl -v -u $login -XPOST -d@layergroup.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/layergroups
